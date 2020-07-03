08f6ffd895f5d5e5aa45806a6a6e0037c3e354b0
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-07-03 12:40:48 +0200

    Added documentation about the package

---
+++ R/RCFF.R
@@ -0,0 +1,24 @@
+#' @title RCFF package metadata
+#' @description RCFF is a package for integrating an R package's DESCRIPTION
+#' file with the universal Citation File Format.
+#' @name RCFF
+#' @author Waldir Leoncio
+#' @docType package
+#' @references
+#' Druskat S., Spaaks J.H., Chue Hong N., Haines R., Baker J. (2019).
+#' Citation File Format (CFF) (version 1.1.0).
+#' DOI: http://doi.org/10.5281/zenodo.1003149
+#'
+#' https://www.software.ac.uk/blog/2017-12-12-standard-format-citation-files
+#' https://citation-file-format.github.io
+#'
+#' @section What is the Citation File Format (CFF)?:
+#' CFF is a standard format for the citation of software proposed by
+#' Stephan Druskat et. al. (see references below). CFF-compliant files are
+#' stored in files named CITATION.cff.
+#'
+#' @section What is a CITATION.cff file?:
+#' CITATION.cff files are plain text files with human- and machine-readable
+#' citation information for software. Code developers can include them in their
+#' repositories to let others know how to correctly cite their software.
+NULL
