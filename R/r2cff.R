2fbc57eaa989a95cf1c6e5cc69c72fd1fe0d3f08
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-04-30 09:00:38 +0200

    No need to pass colon anymore

--- R/r2cff.R
+++ R/r2cff.R
@@ -14,8 +14,8 @@ r2cff <- function(description_file = "DESCRIPTION", export = FALSE) {
 		'message: "If you use this software, please cite it using these metadata."'
 	)
 	for (l in seq_along(desc)) {
-		cff <- append(cff, fetchCFFelement(desc[l], "Title:"))
-		cff <- append(cff, fetchCFFelement(desc[l], "Version:"))
+		cff <- append(cff, fetchCFFelement(desc[l], "Title"))
+		cff <- append(cff, fetchCFFelement(desc[l], "Version"))
 	}
 	if (!export) {
 		return(cat(cff, sep = "\n"))
@@ -26,10 +26,20 @@ r2cff <- function(description_file = "DESCRIPTION", export = FALSE) {
 }
 
 fetchCFFelement <- function(string, element_r, element_cff = tolower(element_r)) {
+	# ======================================================== #
+	# Adding colons if necessary                               #
+	# ======================================================== #
+	element_r <- addColon(element_r)
+	element_cff <- addColon(element_cff)
 	element_r_nchar <- nchar(element_r)
 	if (substr(string, 1, element_r_nchar) == element_r) {
 		element_value <- gsub(paste0(element_r, " "), "", string)
 		cff_entry <- paste(element_cff, element_value)
 		return(cff_entry)
 	}
+}
+
+addColon <- function(txt) {
+	if (substring(txt, nchar(txt)) != ":") txt <- paste0(txt, ":")
+	return(txt)
 }
