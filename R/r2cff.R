ee1b7f8557ca4dd0b90ff2509cf5d9b5a672ea1e
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-06-04 09:48:18 +0200

    Implementex CFF exporting (closes #5)

--- R/r2cff.R
+++ R/r2cff.R
@@ -65,8 +65,20 @@ r2cff <- function(description_file = "DESCRIPTION", export = FALSE) {
 	if (!export) {
 		return(cat(cff, sep = "\n"))
 	} else {
-		# TODO: #5 implement saving feature
-		stop("Exporting not yet available")
+		out_file_name <- "CITATION.cff"
+		if (file.exists(out_file_name)) {
+			out_file_name -> out_file_name_old
+			out_file_name <- tempfile(
+				pattern="CITATION_", tmpdir="", fileext=".cff"
+			)
+			out_file_name <- gsub(pattern="/", replacement="", x=out_file_name)
+			message(
+				out_file_name_old, " already exists. Saving as ", out_file_name
+			)
+		}
+		out_file <- file(out_file_name)
+		writeLines(cff, out_file)
+		close(out_file)
 	}
 }
 
