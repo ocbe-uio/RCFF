01b9337e0cf9f8e5b4d7f15bdf43aa78dde5a75d
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-06-04 10:20:01 +0200

    Moved exporting feature out of main

--- R/r2cff.R
+++ R/r2cff.R
@@ -32,13 +32,13 @@ r2cff <- function(description_file = "DESCRIPTION", export = FALSE) {
 		cff <- append(
 			cff,
 			fetchCFFelement(
-				desc[l], "person",  "  -", add_colons=FALSE, element_value=""
+				desc[l], "person", "  -", add_colons=FALSE, element_value=""
 			)
 		)
 		cff <- append(
 			cff,
 			fetchCFFelement(
-				desc[l], "given =",  "    given-names:", add_colons=FALSE,
+				desc[l], "given =", "    given-names:", add_colons=FALSE,
 				remove_elements=c('"', ",")
 			)
 		)
@@ -60,20 +60,8 @@ r2cff <- function(description_file = "DESCRIPTION", export = FALSE) {
 	if (!export) {
 		return(cat(cff, sep = "\n"))
 	} else {
-		out_file_name <- "CITATION.cff"
-		if (file.exists(out_file_name)) {
-			out_file_name -> out_file_name_old
-			out_file_name <- tempfile(
-				pattern="CITATION_", tmpdir="", fileext=".cff"
-			)
-			out_file_name <- gsub(pattern="/", replacement="", x=out_file_name)
-			message(
-				out_file_name_old, " already exists. Saving as ", out_file_name
-			)
-		}
-		out_file <- file(out_file_name)
-		writeLines(cff, out_file)
-		close(out_file)
+		exportCFF(cff)
+
 	}
 }
 
@@ -111,4 +99,17 @@ remove_elements=NULL) {
 addColon <- function(txt) {
 	if (substring(txt, nchar(txt)) != ":") txt <- paste0(txt, ":")
 	return(txt)
+}
+
+exportCFF <- function(infile, outfile="CITATION.cff") {
+	outfile <- "CITATION.cff"
+	if (file.exists(outfile)) {
+		outfile -> outfile_old
+		outfile <- tempfile(pattern="CITATION_", tmpdir="", fileext=".cff")
+		outfile <- gsub(pattern="/", replacement="", x=outfile)
+		message(outfile_old, " already exists. Saving as ", outfile)
+	}
+	out_file <- file(outfile)
+	writeLines(infile, out_file)
+	close(out_file)
 }
